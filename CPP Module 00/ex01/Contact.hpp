16012f42391ae08801e23edfdc5799e891661f3c
mechane <medyokr@gmail.com>
2023-07-24 09:51:40 +0100

    create classes

---
+++ CPP Module 00/ex01/Contact.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 09:34:26 by mechane           #+#    #+#             */
+/*   Updated: 2023/07/24 09:49:33 by mechane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+#include <string>
+
+class Contact{
+private :
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phonenumber;
+	std::string darkest_secret;
+
+	
+};
+#endif 
---
+++ CPP Module 00/ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 09:22:53 by mechane           #+#    #+#             */
+/*   Updated: 2023/07/24 09:43:25 by mechane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+# include "Contact.hpp"
+
+class PhoneBook{
+private :
+	Contact contacts[8];
+	
+};
+
+#endif
