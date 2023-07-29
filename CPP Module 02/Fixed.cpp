d10097d81264b35d2278400a1b534b21dfbee369
mechane <medyokr@gmail.com>
2023-07-29 12:40:47 +0100

    dn

--- CPP Module 02/Fixed.cpp
+++ CPP Module 02/Fixed.cpp
@@ -6,8 +6,30 @@
 /*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 10:08:01 by mechane           #+#    #+#             */
-/*   Updated: 2023/07/28 10:09:08 by mechane          ###   ########.fr       */
+/*   Updated: 2023/07/29 11:34:09 by mechane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed() : _n(0)
+{
+	std::cout  << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+void 	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_n = raw;
+}
+
+int		Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_n;	
+}
--- CPP Module 02/Fixed.hpp
+++ CPP Module 02/Fixed.hpp
@@ -6,13 +6,24 @@
 /*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 10:08:07 by mechane           #+#    #+#             */
-/*   Updated: 2023/07/28 10:08:50 by mechane          ###   ########.fr       */
+/*   Updated: 2023/07/29 11:31:41 by mechane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <iostream>
+class Fixed
+{
+	int	_n;
+	static const int _b = 8;	
+public :
+	Fixed();
+	~Fixed();
+	void setRawBits(int const raw);
+	int getRawBits(void) const;
 
-
+	
+};
 #endif
