edec28ca4dfc7f0417edac7bf739215af1c01dcc
mechane <medyokr@gmail.com>
2023-08-30 09:45:46 +0100

    done

--- CPP Module 09/ex00/BitcoinEchange.hpp
+++ CPP Module 09/ex00/BitcoinEchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 10:33:16 by mechane           #+#    #+#             */
-/*   Updated: 2023/08/29 10:11:57 by mechane          ###   ########.fr       */
+/*   Updated: 2023/08/30 09:39:50 by mechane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,3 @@ class BitcoinEchange
 		BitcoinEchange & operator=(const BitcoinEchange &assign);
 
 };
-   
